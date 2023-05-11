59aa53e680f14967a6ece4dfce7bc6bec81aafe1
Cameron Tacklind <cameron@tacklind.com>
2023-05-10 20:13:19 -0700

    Remove orphaned file

--- src/Progress.ts
+++
@@ -1,7 +0,0 @@
-export class Progress {
-  private seq: number = 0;
-
-  spin(count: number, total: number): void {
-    process.stdout.write(`\r${'-\\|/'[this.seq++ % 4]} ${~~((count * 100) / total)}%\r`);
-  }
-}
